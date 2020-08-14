8216b76b48372c4e2d785fac8414161c8c01eda7
AkshithReddy22 <69467922+AkshithReddy22@users.noreply.github.com>
2020-08-14 13:03:04 +0530

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,1 +1,25 @@
+a=input("Enter a sentence")
+count=0
+alpha='a'
+b={}
+for i in range(0,26):
+    b[alpha]=count
+    alpha=chr(ord(alpha)+1)
+a=a.lower()
+for i in a:
+    b[i]=b[i]+1
+def function(b):
+    for i in range(ord('a'),ord('z')+1):
+        if(b[chr(i)]>=1):
+            return True
+        else:
+            print("This is not pangram")
+            break
+function(b)
+if(function==True):
+    print("This is pangram")
 
+        
+  
+  
+  
